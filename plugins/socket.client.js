31f750fe135fc10ab1196dcaeadde8689472f12e
Roman Krutarchuk <makarenaclubg@gmail.com>
2024-04-21 16:24:48 +0200

    url

--- plugins/socket.client.js
+++ plugins/socket.client.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 const URL =
   process.env.NODE_ENV === "production"
-    ? process.env.SOCKET_API_URL
+    ? "https://api-1-0-0fru.onrender.com/"
     : "http://localhost:3080";
 const socket = io(URL);
 socket.on("hello", (data) => {
