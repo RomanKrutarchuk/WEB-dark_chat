7aa7260af9d16679b115fa7f82152b48048746a5
Roman <makarenaclubg@mail.com>
2024-05-10 21:46:17 +0200

    route guard befor

--- middleware/routeAuthGuard.global.js
+++ middleware/routeAuthGuard.global.js
@@ -1,20 +1,28 @@
 import { useAuthStore } from "~/store/user/auth";
 
 export default defineNuxtRouteMiddleware((to, from) => {
-  const { $socket } = useNuxtApp()
-  const connected = $socket.connected
-  if (connected) {
-    const { getId } = useAuthStore()
-    const id = getId()
-    if (!id) {
-      console.log('middleware.router-guard.missing-id');
-      if (to.fullPath != "/authentication") {
-        return navigateTo("/authentication");
-      }
-    }
-  } else {
-    if (to.fullPath != "/error") {
-      return navigateTo("/error")
+  const { getId } = useAuthStore()
+  const id = getId()
+  if (!id) {
+    console.log('middleware.router-guard.missing-id');
+    if (to.fullPath != "/authentication") {
+      return navigateTo("/authentication");
     }
   }
+  // const { $socket } = useNuxtApp()
+  // const connected = $socket.connected
+  // if (connected) {
+  //   const { getId } = useAuthStore()
+  //   const id = getId()
+  //   if (!id) {
+  //     console.log('middleware.router-guard.missing-id');
+  //     if (to.fullPath != "/authentication") {
+  //       return navigateTo("/authentication");
+  //     }
+  //   }
+  // } else {
+  //   if (to.fullPath != "/error") {
+  //     return navigateTo("/error")
+  //   }
+  // }
 });
